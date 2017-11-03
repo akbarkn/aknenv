38502826aa65be5fb85d7716269596402e133737
Akbar Kusuma Negara <akbarkusumanegaralth@gmail.com>
2017-11-03 17:00:43 +0700

    add struct

--- env.go
+++ env.go
@@ -9,21 +9,46 @@ import (
 	"strings"
 )
 
-type EnvPath string
-
-var ep EnvPath
-
-func SetPath(path string) {
-	var pt EnvPath
-	pt = EnvPath(path)
-	ep.Pathing(pt)
-}
-func (e *EnvPath) Pathing(str EnvPath) {
-	*e = str
+type Env struct {
+	Path string
 }
 
-func GetPath() string {
-	return string(ep)
+// type EnvPath string
+
+// var ep EnvPath
+
+// func SetPath(path string) {
+// 	var pt EnvPath
+// 	pt = EnvPath(path)
+// 	ep.Pathing(pt)
+// }
+// func (e *EnvPath) Pathing(str EnvPath) {
+// 	*e = str
+// }
+
+func (e *Env) Set(str string) *Env {
+	e.Path = str
+	return e
+}
+
+func (e *Env) GetEnv(str string) string {
+	if len(getSystemVariable(str)) > 0 {
+		return getSystemVariable(str)
+	}
+	filePath := filepath.Join(e.Path, ".env")
+	env, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Println(fmt.Sprintf("Variable %s is not found anywhere", str))
+		return ""
+	}
+	arr := envToArray(string(env))
+	for _, v := range arr {
+		slice := sliceValue(v)
+		if strings.Compare(slice[0], str) == 0 {
+			return slice[1]
+		}
+	}
+	return ""
 }
 
 func GetEnv(str string, path ...string) string {
--- env_test.go
+++ env_test.go
@@ -40,3 +40,22 @@ func TestVariableValue2(t *testing.T) {
 	}
 	t.Logf("SUCCESS")
 }
+
+func TestVariableValue3(t *testing.T) {
+	env := Env{}
+	if strings.Compare(env.Set("./test").GetEnv("DB_HOST"), "192.168.1.2") != 0 {
+		t.Errorf("VARIABLE VALUE 1 IS INCORRECT, SHOULD BE %s", env.Set("./test").GetEnv("DB_HOST"))
+		t.Fail()
+		return
+	} else {
+		t.Logf("VARIABLE VALUE 1 IS CORRECT")
+	}
+	if strings.Compare(env.Set("./test").GetEnv("DB_PORT"), "3307") != 0 {
+		t.Errorf("VARIABLE VALUE 2 IS INCORRECT, SHOULD BE %s", env.Set("./test").GetEnv("DB_PORT"))
+		t.Fail()
+		return
+	} else {
+		t.Logf("VARIABLE VALUE 2 IS CORRECT")
+	}
+	t.Logf("SUCCESS")
+}
